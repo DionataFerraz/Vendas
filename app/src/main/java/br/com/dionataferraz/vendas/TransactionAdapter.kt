4fcedca070633c4cda1a52b73fa3022ad3c9a4ad
João Dier <joaomeleski@icloud.com>
2022-09-18 22:13:48 -0300

    transactions

--- app/src/main/java/br/com/dionataferraz/vendas/TransactionAdapter.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/TransactionAdapter.kt
@@ -1,9 +1,14 @@
-package br.com.dionataferraz.vendas
+package br.com.dionataferraz.vendas.transactions
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.dionataferraz.vendas.R
+import br.com.dionataferraz.vendas.TransactionItem
+import br.com.dionataferraz.vendas.TransactionType
 import br.com.dionataferraz.vendas.databinding.ItemListBinding
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 class TransactionAdapter(private val listener: Listener) :
     RecyclerView.Adapter<TransactionViewHolder>() {
@@ -12,7 +17,7 @@ class TransactionAdapter(private val listener: Listener) :
         fun onItemClick(text: String)
     }
 
-    private val listItem: MutableList<String> = mutableListOf()
+    private val listItem: MutableList<TransactionItem> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
         val binding = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,18 +32,9 @@ class TransactionAdapter(private val listener: Listener) :
     override fun getItemCount(): Int {
         return listItem.size
     }
-    fun addNewList(list: List<String>) {
-        listItem.clear()
-        notifyItemRangeRemoved(0, listItem.size)
-        listItem.addAll(list)
-    }
 
-    fun addList(list: List<String>) {
+    fun addList(list: List<TransactionItem>) {
         listItem.addAll(list)
-    }
-    fun updateItem(item: String, position: Int) {
-        listItem[position] = item
-        notifyItemChanged(position)
     }
 
 }
@@ -46,12 +42,26 @@ class TransactionAdapter(private val listener: Listener) :
 class TransactionViewHolder(
     private val binding: ItemListBinding,
     private val listener: TransactionAdapter.Listener
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(name: String) {
-        binding.tvName.text = name
+    private val formatter = SimpleDateFormat("hh:mm")
+
+    fun bind(TransactionItem: TransactionItem) {
+        val value = TransactionItem.price.toString()
+
+        binding.tvTransactionTitle.text = TransactionItem.title
+        binding.tvTransactionTime.text = formatter.format(TransactionItem.time)
+        binding.tvTransactionValue.text = "R$ $value"
+
+        when (TransactionItem.type) {
+            TransactionType.SERVICES_ON_DEMAND -> binding.ivTransaction.setImageResource(R.drawable.ic_baseline_video_library_24)
+            TransactionType.GAS_STATION -> binding.ivTransaction.setImageResource(R.drawable.ic_baseline_local_gas_station_24)
+            TransactionType.MARKET -> binding.ivTransaction.setImageResource(R.drawable.ic_baseline_shopping_cart_24)
+            TransactionType.BAR -> binding.ivTransaction.setImageResource(R.drawable.ic_baseline_local_bar_24)
+        }
+
         binding.root.setOnClickListener {
-            listener.onItemClick(name)
+            listener.onItemClick(TransactionItem.title)
         }
     }
 }
